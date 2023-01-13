8f53d2ea0fd0cd6520b5d4f540b79075a493d0cd
anmendoza24 <anmendoza@student.apc.edu.ph>
2023-01-13 08:25:33 +0800

    JS Print button

--- Week5/myscript.js
+++ Week5/myscript.js
@@ -3,3 +3,7 @@ alert('Dont Open this website if youre not pogi ')
 function myFunction() {
     document.getElementById("demo").innerHTML = "Paragraph changed.";
  }
+
+ function myFunction() {
+    document.getElementById("Addition").innerHTML = 5 + 6;
+ }
