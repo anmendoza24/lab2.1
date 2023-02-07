72b9619c0ef8f9951624cd1a35d49b27f28cf22f
anmendoza24 <anmendoza@student.apc.edu.ph>
2023-02-07 09:29:17 +0800

    change dbname

--- week9/guest.php
+++ week9/guest.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mydbmi211";
+$dbname = "mydbwebprogmi211";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
