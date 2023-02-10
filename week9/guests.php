497086c1805df31e4a250971775879ac8fe66e8e
anmendoza24 <anmendoza@student.apc.edu.ph>
2023-02-10 08:34:31 +0800

    Hopefully Fixed guests.php

--- week9/guests.php
+++ week9/guests.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while ($row = $result->fetch_assoc()) {
-    echo " <br> id: " . $row["id"]. " - Name: " . $row["fname"]. " - Email: " . $row["email"]. " - Website :" . $row["website"]. " - Comment : ". $row["comment"].  "<br>";
+    echo " - Name: " . $row["fname"]. " - Email: " . $row["email"]. " - Website :" . $row["website"]. " - Comment : ". $row["comment"].  "<br>";
   }
 } else {
   echo "0 results";
