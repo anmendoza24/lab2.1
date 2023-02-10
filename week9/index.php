3f4df4f95ffdb30088efec2fc606f13ca4b465c8
anmendoza24 <anmendoza@student.apc.edu.ph>
2023-02-10 08:19:18 +0800

    SQL Insert Updated

--- week9/index.php
+++ week9/index.php
@@ -203,7 +203,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	die("Connection failed: " . $conn->connect_error);
 	}
 	
-	$sql = "INSERT INTO myguests (fname,email,website,comment,gender)
+	$sql = "INSERT INTO anmendoza_myguests (fname,email,website,comment,gender)
 	VALUES ('$name','$email', '$website', '$comment', '$gender')";
 	
 	if ($conn->query($sql) === TRUE) {
